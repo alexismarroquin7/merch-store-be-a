7bf6409cce22c4ef9882f8ce68a7d4ceb5229426
alexismarroquin7 <alexismarroquin754@gmail.com>
2022-01-23 16:03:56 -0800

    adds router to api router

--- api/api-router.js
+++ api/api-router.js
@@ -4,12 +4,14 @@ const categoriesRouter = require('./categories/categories-router');
 const sub_categoriesRouter = require('./sub_categories/sub_categories-router');
 const imagesRouter = require('./images/images-router');
 const productsRouter = require('./products/products-router');
+const inventoryRouter = require('./inventory/inventory-router');
 
 router.use('/genders', gendersRouter);
 router.use('/categories', categoriesRouter);
 router.use('/sub_categories', sub_categoriesRouter);
 router.use('/images', imagesRouter);
 router.use('/products', productsRouter);
+router.use('/inventory', inventoryRouter);
 
 router.use((err, req, res, next) => { // eslint-disable-line
   res.status(err.status||500).json({
